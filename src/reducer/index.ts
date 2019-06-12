a1676746cf309edff6f936e41ea9d175e8cdef1e
Srikanth <srikanthjviru@gmail.com>
2019-06-11 23:12:28 -0500

    changes

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -2,7 +2,7 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { reducer as root } from './root';
 
-// Enable redux dev tool
+//Redux thunk integration
 const middleware = compose(applyMiddleware(thunk));
 
 // Create redux store
